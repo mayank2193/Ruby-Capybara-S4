5fc93c289726136c1173b1bb0d1542b8b07f67da
mayank2193 <mayankmaurya2193@gmail.com>
2025-01-07 07:31:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# capybara-Lambdatest
+
+## Setup
+* Clone the repo
+* Install dependencies `bundle install`
+
+
+### Running your tests
+* To run tests, run `bundle exec rake single`
+
+ 
---
+++ features/support/lambdatest.rb
@@ -0,0 +1,41 @@
+require 'yaml'
+require 'selenium/webdriver'
+require 'capybara/cucumber'
+
+# monkey patch to avoid reset sessions
+class Capybara::Selenium::Driver < Capybara::Driver::Base
+  def reset!
+    @browser.navigate.to('about:blank') if @browser
+  end
+end
+
+TASK_ID = (ENV['TASK_ID'] || 0).to_i
+CONFIG_NAME = ENV['CONFIG_NAME'] || 'single'
+
+CONFIG = YAML.safe_load(File.read(File.join(File.dirname(__FILE__), "../../config/#{CONFIG_NAME}.config.yml")))
+CONFIG['user'] = ENV['LT_USERNAME'] || CONFIG['user']
+CONFIG['key'] = ENV['LT_ACCESS_KEY'] || CONFIG['key']
+
+Capybara.register_driver :lambdatest do |app|
+ 
+  options = Selenium::WebDriver::Chrome::Options.new
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      "browserName"=>"chrome", 
+      "version"=>"latest",
+      "build"=>"capybara-lambdatest",
+      "name"=>"capybara-lambdatest",
+      "video"=>true,
+      "network"=>true,
+      "console"=>true,
+      "visual"=>true 
+  )
+
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :remote,
+                                 url: "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
+                                 options: capabilities)
+end
+
+Capybara.default_driver = :lambdatest
+Capybara.run_server = false
+
